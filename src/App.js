3ea409a99f13b6f249a07bf19e334596e486eea1
airplane22 <2002airplane4@gmail.com>
2022-03-10 14:56:01 +0900

    ~#6.4 cleanup

--- src/App.js
+++ src/App.js
@@ -2,38 +2,23 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+
+
+function Hello() {
+  useEffect(() => { 
+    console.log("created!"); // runs only when component is first created
+    return () => console.log("destroyed:("); // cleanup function : runs when component is destroyed, not used very often
+  }, []); 
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyWord] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyWord(event.target.value);
-  console.log('render');
-  useEffect(()=> {
-    console.log("I run only once");
-  }, []); // watch nothing, only change once(at first load)
-  useEffect(()=> {
-    if (keyword !=="" && keyword.length > 6) { //??? 
-      console.log("I run when 'keyword changes", keyword);
-    }
-  }, [keyword]); // keyword 변화할 때만 실행 - run only when keyword changes
-
-  useEffect(()=> {
-      console.log("I run when 'counter' changes");
-  }, [counter]);
-
-  useEffect(()=>{
-    console.log("I run when 'keyword' and 'counter' changes");
-  }, [keyword, counter]);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev)=>!prev);
   return (
     <div>
-      <input 
-      value={keyword}
-      onChange={onChange} 
-      type="text" 
-      placeholder="Search here"></input>
-      <h1 >{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing?<Hello/> : null}
+      <button onClick={onClick}>{showing? "hide" : "show"}</button>
     </div>
   );
 }
