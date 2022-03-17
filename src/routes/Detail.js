13912dd22de123f1770d210edc9f926f6ae860cd
airplane22 <2002airplane4@gmail.com>
2022-03-17 21:30:16 +0900

    #7.6 datil page modified

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,21 +1,41 @@
-import { getMouseEventOptions } from "@testing-library/user-event/dist/utils";
-import { useEffect } from "react";
-import {useParams} from "react-router-dom"
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import Movie from "../components/Movie";
+
 
 function Detail() {
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
     const { id } = useParams();
     const getMovie = async () => {
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
-        console.log(json);
+        setMovie(json.data.movie);
+        setLoading(false);
     };
     useEffect(()=> {
         getMovie();
     }, [])
-    return <h1>Detail</h1>;
+    return (
+        <div>
+            { loading ?
+            <h1> Loading... </h1> 
+            :
+            <Movie 
+            key={movie.id}
+            id={movie.id}
+            coverImg={movie.medium_cover_image} 
+            title = {movie.title}
+            summary = {movie.summary}
+            genres = {movie.genres}
+            /> 
+            }
+        </div> 
+    )
 }
 
 export default Detail;
 
-//로딩, detail page, state, navigation bar, home, about 
+//detail page, state, navigation bar, home, about 
